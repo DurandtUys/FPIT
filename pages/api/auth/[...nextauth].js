a24f9060e9192e4e73b5067b4aa6572c77993a54
Durandt Uys <u18286250@tuks.co.za>
2022-09-28 22:13:57 +0200

    remove session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -46,13 +46,6 @@ const callbacks = {
 
     return token;
   },
-
-  async session({ session, token }) {
-    if (Date.now() >= token.user.exp * 1000) return null;
-    session.accessToken = token.accessToken;
-    session.user = token.user;
-    return session;
-  },
 };
 
 export const options = {
